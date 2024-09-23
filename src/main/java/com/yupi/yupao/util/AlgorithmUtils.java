1efbf8999299656e9ad0bd1ef7558d8c13bd2ecb
wangzhenzhou <1501920237@qq.com>
2024-09-23 12:17:25 +0800

    编辑距离修改。

--- src/main/java/com/yupi/yupao/util/AlgorithmUtils.java
+++ src/main/java/com/yupi/yupao/util/AlgorithmUtils.java
@@ -1,66 +1,76 @@
 package com.yupi.yupao.util;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 
 /**
  * 算法工具类
- *
- * 
- * 
  */
 public class AlgorithmUtils {
 
     /**
      * 编辑距离算法（用于计算最相似的两组标签）
      * 原理：https://blog.csdn.net/DBC_121/article/details/104198838
-     *
+     * by zz: 这里不应该用编辑举例，因为这里的参数只是一组标签，虽然用List装着，但实际上这些标签业务上之间没有顺序关系！
+     *        因此，修改算法为set的并集-set交集，即是距离。
      * @param tagList1
      * @param tagList2
      * @return
      */
     public static int minDistance(List<String> tagList1, List<String> tagList2) {
-        int n = tagList1.size(); // 3
-        int m = tagList2.size(); // 4
-
-        if (n * m == 0) {
-            return n + m;
-        }
-
-        int[][] d = new int[n + 1][m + 1]; // [4][5] [i][j]代表 list1 i-1之前，list2 j-1 之前的相似度
-        for (int i = 0; i < n + 1; i++) {  // 边界，最大
-            d[i][0] = i;
-        }
-
-        for (int j = 0; j < m + 1; j++) {  // 边界，最大
-            d[0][j] = j;
-        }
-        // i-1，j-1 子串 的距离不会超过i或 不会超过j
-
-        // a b c       d      a
-        //             c      b
-        //             b      c
-        //             a      d
-        //
-        // 0 1 2 3 4         0 1 2 3 4
-        // 1 1               1 0 1 2 3
-        // 2                 2
-        // 3                 3
+        Set<String> union = new HashSet<>(tagList1);
+        Set<String> intersection = new HashSet<>(tagList1);
+        union.addAll(tagList2);
+        intersection.retainAll(tagList2);
+        return union.size()-intersection.size();
 
 
-        for (int i = 1; i < n + 1; i++) { // 行遍历
-            for (int j = 1; j < m + 1; j++) {
-                int left = d[i - 1][j] + 1; // up
-                int down = d[i][j - 1] + 1;// left
-                int left_down = d[i - 1][j - 1]; // up and left
-                if (!Objects.equals(tagList1.get(i - 1), tagList2.get(j - 1))) {  // [i][j]
-                    left_down += 1;
-                }
-                d[i][j] = Math.min(left, Math.min(down, left_down));
-            }
-        }
-        return d[n][m];
+//        int n = tagList1.size(); // 3
+//        int m = tagList2.size(); // 4
+//
+//        if (n * m == 0) {
+//            return n + m;
+//        }
+//
+//        int[][] d = new int[n + 1][m + 1]; // [4][5] [i][j]代表 list1 i-1之前，list2 j-1 之前的相似度
+//        for (int i = 0; i < n + 1; i++) {  // 边界，最大
+//            d[i][0] = i;
+//        }
+//
+//        for (int j = 0; j < m + 1; j++) {  // 边界，最大
+//            d[0][j] = j;
+//        }
+//        // i-1，j-1 子串 的距离不会超过i或 不会超过j
+//        // tagList1  tagList2   tagList2
+//        //         d c b a         a b c d
+//        // a
+//        // b
+//        // c
+//        //
+//        //
+//        // 0 1 2 3 4             0 1 2 3 4
+//        // 1 1 2 3 3                   1 0 1 2 3
+//        // 2 2 2                    2
+//        // 3 3                     3
+//
+//        // (...)a  (...)b  依赖于(...) (...)+1、 (...)a (...)+1、（...） (...)b +1;
+//        // (...)a  (...)a       (...) (...)、  (...)a  (...)、(...)  (...)a
+//
+//        for (int i = 1; i < n + 1; i++) { // 行遍历
+//            for (int j = 1; j < m + 1; j++) {
+//                int left = d[i - 1][j] + 1; // up + 1
+//                int down = d[i][j - 1] + 1;// left + 1
+//                int up_left = d[i - 1][j - 1]; // up and left
+//                if (!Objects.equals(tagList1.get(i - 1), tagList2.get(j - 1))) {  // [i][j]
+//                    d[i][j] = Math.min(left, Math.min(down, up_left+1));
+//                } else {
+//                    d[i][j] = d[i - 1][j - 1];
+//                }
+//            }
+//        }
+//        return d[n][m];
     }
 
 
@@ -93,18 +103,26 @@ public class AlgorithmUtils {
             for (int j = 1; j < m + 1; j++) {
                 int left = d[i - 1][j] + 1;
                 int down = d[i][j - 1] + 1;
-                int left_down = d[i - 1][j - 1];
+                int left_up = d[i - 1][j - 1];
                 if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
-                    left_down += 1;
+                    d[i][j] = Math.min(left, Math.min(down, left_up+1));
+                } else {
+                    d[i][j] = d[i - 1][j - 1];
                 }
-                d[i][j] = Math.min(left, Math.min(down, left_down));
             }
         }
+//        for (int i = 0; i < n+1; i++) {
+//            for (int j = 0; j < m+1; j++) {
+//                System.out.print(d[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
         return d[n][m];
     }
 
     /**
      * 判断对象中除某个字段（比如id）和serialVersion字段外其他字段是否为null
+     *
      * @param obj
      * @param idFieldName
      * @return
@@ -113,7 +131,7 @@ public class AlgorithmUtils {
         Field[] fields = obj.getClass().getDeclaredFields();
 
         for (Field field : fields) {
-            if (!field.getName().equals(idFieldName) && !field.getName().equals("serialVersionUID") ) {
+            if (!field.getName().equals(idFieldName) && !field.getName().equals("serialVersionUID")) {
                 field.setAccessible(true); // 确保我们可以访问private字段
                 try {
                     Object value = field.get(obj);
--- src/test/java/com/yupi/yupao/YupaoBackendDuplicateApplicationTests.java
+++ src/test/java/com/yupi/yupao/YupaoBackendDuplicateApplicationTests.java
@@ -1,18 +1,28 @@
 package com.yupi.yupao;
 
+import com.yupi.yupao.util.AlgorithmUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.ArrayList;
 
 @SpringBootTest
 class YupaoBackendDuplicateApplicationTests {
 
     @Test
     void contextLoads() {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
 
+        ArrayList<String> strings2 = new ArrayList<>();
+        strings2.add("d");
+        strings2.add("c");
+        strings2.add("b");
+        strings2.add("a");
+
+        System.out.println(AlgorithmUtils.minDistance(strings2,strings));
     }
 
 
